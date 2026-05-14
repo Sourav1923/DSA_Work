1fdbd10542a270a08692872aacf5fda390abf3c0
Sourav1923 <souravmahapatra213@gmail.com>
2026-05-14 18:12:26 +0530

    OOP2

---
+++ untitled/src/com/O_OOPs/Packages/Pkg_a/Greeting.java
@@ -0,0 +1,9 @@
+package com.O_OOPs.Packages.Pkg_a;
+import static com.O_OOPs.Packages.Pkg_b.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        message();
+    }
+}
---
+++ untitled/src/com/O_OOPs/Packages/Pkg_b/Message.java
@@ -0,0 +1,10 @@
+package com.O_OOPs.Packages.Pkg_b;
+
+public class Message {
+    public static void main(String[] args) {
+
+    }
+    public static void message(){
+        System.out.println("This is Best Java Course");
+    }
+}
